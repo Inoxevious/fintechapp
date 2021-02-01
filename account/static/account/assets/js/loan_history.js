3c6cfc1128caac021b11b7481d607674e144ecb7
Inoxevious <mpasiinnocent@gmail.com>
2021-02-01 12:54:13 +0200

    ecs commit

--- account/static/account/assets/js/loan_history.js
+++ account/static/account/assets/js/loan_history.js
@@ -145,7 +145,23 @@ function putTableData(result) {
                 "<td>" + b.ORGANIZATION_TYPE + "</td>" +
                 "<td>" + b.OCCUPATION_TYPE + "</td>" +
                 "<td>" + b.NAME_INCOME_TYPE + "</td>" +
-                "<td style='background-color:" + color + ";'>" + b.LIVINGAREA_AVG + "</td></tr>"
+                "<td style='background-color:" + color + ";'>" + b.LIVINGAREA_AVG + "</td>
+                    `<td class="text-center">
+                    <ul class="icons-list">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="icon-menu9"></i>
+                            </a>
+
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li><a href="#"><i class="icon-file-pdf"></i> Export to .pdf</a></li>
+                                <li><a href="#"><i class="icon-file-excel"></i> Export to .csv</a></li>
+                                <li><a href="#"><i class="icon-file-word"></i> Export to .doc</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </td>
+                </tr>`
             $("#listing").append(row);
         });
     }
