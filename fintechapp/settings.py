1db4fded8d500b05ead2b882d944340ab2c8e203
Inoxevious <mpasiinnocent@gmail.com>
2021-02-06 00:08:44 +0200

    p

--- fintechapp/settings.py
+++ fintechapp/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_FILES = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
